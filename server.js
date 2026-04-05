29a29bec38171f80897bec40e5188142b05473c4
vaibhavjangam001 <vaibhavjangam001@gmail.com>
2026-04-05 17:42:40 +0530

    add new file server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+
+import http from "http";
+import fs from "fs";
+
+const PORT = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/Todos" && req.method === "GET") {
+    const data = fs.readFileSync("./Todo.json", "utf-8");
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
